7b7c19682fa56e2b973947f4f89731579872009c
Aurelien heude <aurelienheudepro@gmail.com>
2021-09-22 15:00:58 +0200

    debut du projet

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Heude Conception</title>
+
+    <link href="./assets/font_awesome/css/all.css" rel="stylesheet">
+    <!--load all styles -->
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <nav class="menu">
+            <div class="left_toolbar">
+                <input type="search" id="site-search" name="q" aria-label="Search through site content">
+                <i class="fas fa-search fa-1x"></i>
+                
+            </div>
+            <div class="right_toolbar">
+                <div class="notification">
+                    <i class="far fa-bell fa-1x"></i>
+                    <i class="fas fa-chevron-down fa-1x"></i>
+                </div>
+                <div class="settings">
+                    <i class="fas fa-cog fa-1x"></i>
+                    <i class="fas fa-chevron-down fa-1x"></i>
+                </div>
+
+            </div>
+        </nav>
+
+    </div>
+
+</body>
+
+</html>
