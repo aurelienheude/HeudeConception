197a6388de9ffaf16113b5d732e81f3c8fb76bd0
Aurelien heude <aurelienheudepro@gmail.com>
2021-09-29 23:11:33 +0200

    modif

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice</title>
+
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="font_awesome/css/all.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="left">
+            bloc menu
+        </div>
+        <div class="right">
+            <div class="nav">
+                <div class="left_nav">
+                    <input type="search" placeholder="  recherche..." id="site-search" name="q"
+                        aria-label="Search through site content">
+                    <!-- <i class="fas fa-search fa-1x"></i> -->
+                </div>
+
+                <div class="right_nav">
+                    <div>
+                        <a href="">
+                            <i class="far fa-bell fa-1x"></i>
+                            <i class="fas fa-chevron-down fa-1x"></i>
+                        </a>
+                    </div>
+                    <div>
+                        <i class="fas fa-cog fa-1x"></i>
+                        <i class="fas fa-chevron-down fa-1x"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="line_cards">
+                <div class="cards">
+                    <div class="header_cards">
+                        <b>Vente journalière</b>
+                    </div>
+                    <div class="article_cards">
+                        <i class="fas fa-arrow-up fa-2x"></i>
+                        <b> 249.95 €</b>
+                    </div>
+                    <div class="progress_cards">
+                        <progress class="progress" id="file" max="100" value="70"> 70% </progress>
+                    </div>
+                </div>
+                <div class="cards">
+                    <div class="header_cards">
+                        <b>Ventes Mensuelle</b>
+                    </div>
+                    <div class="article_cards">
+                        <b>2451.21 €</b>
+                    </div>
+                    <div class="progress_cards">
+                        <progress class="progress" id="file" max="100" value="70"> 70% </progress>
+                    </div>
+                </div>
+                <div class="cards">
+                    <div class="header_cards">
+                        <b>Ventes Annuelle</b>
+                    </div>
+                    <div class="article_cards">
+                        <b>46785.54 €</b>
+                    </div>
+                    <div class="progress_cards">
+                        <progress class="progress" id="file" max="100" value="70"> 70% </progress>
+                    </div>
+                </div>
+            </div>
+
+            <div class="line_cards_2">
+                <div class="stat_cards">
+                    a
+                </div>
+
+                <div class="event_incoming_cards">
+                    a
+                </div>
+
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
